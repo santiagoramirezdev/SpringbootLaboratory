b89b60ee4cd991026cbb1b2a80b033fdd2cc7cf1
Santiago Ramirez Pinilla <santiagoxrp@gmail.com>
2024-12-08 16:11:55 -0500

    inicializando proyecto con springboot

---
+++ hola-mundo/src/main/java/hola_mundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package hola_mundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
